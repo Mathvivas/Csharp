99ac4d20f6cc29405b1c258be525ee856e075729
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-01-22 16:46:45 -0300

    Update

--- Instructions/Program.cs
+++ Instructions/Program.cs
@@ -6,7 +6,33 @@ namespace Instructions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int idade;
+            //int b = 2, c = 5;
+            //const double d = 8.5;
+            string loop = "sim";
+
+            do 
+            {
+
+                Console.WriteLine("Digite seu nome: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Digite sua idade: ");
+                idade = Convert.ToInt16(Console.ReadLine());
+
+                if ( name.Length == 0 ) 
+                {
+                    Console.WriteLine("\nNome não foi informado!");
+
+                } else {
+                    Console.WriteLine($"{name} possui {idade} anos");
+                }
+
+                Console.WriteLine("\n\nDeseja reinformar os valores: ");
+                loop = Console.ReadLine().ToLower();
+
+
+            } while (loop == "sim");
+
         }
     }
 }
