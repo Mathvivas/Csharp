aa084494e51dffcdfc17b8b080921e05cc081032
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-01-22 15:28:20 -0300

    Update Estrutura

--- Estrutura/Program.cs
+++ Estrutura/Program.cs
@@ -6,7 +6,16 @@ namespace Estrutura
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Utilizar BreakPoints para fazer um passo-a-passo.
+
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
         }
     }
 }
