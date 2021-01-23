f11197b75f734ad016cb1e8a6c2b9e360e84e00a
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-01-23 19:58:17 -0300

    Criação do Struct Aluno

---
+++ Alunos/Aluno.cs
@@ -0,0 +1,9 @@
+namespace Alunos
+{
+    public struct Aluno     // Struct --> Estrutura mais simples
+    {
+        public string Nome { get; set; }    // prop + TAB
+
+        public decimal Nota { get; set; }
+    }
+}
