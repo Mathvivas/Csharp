7991f05fc5a540e295a0d3d6f59eb45b13c4f652
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-01-24 00:32:49 -0300

    Update

--- Gasolina/Program.cs
+++ Gasolina/Program.cs
@@ -6,7 +6,19 @@ namespace Gasolina
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string entrada = "10 85";	// 1°) Tempo (horas)    2°) Velocidade (km/h)
+    
+            string[] entradaLista = new string[2];
+            entradaLista = entrada.Split(" ");
+
+            float tempo = float.Parse(entradaLista[0]);
+            float vel = float.Parse(entradaLista[1]);
+            
+            float km = vel * tempo;             // Distância (km)
+            
+            float gas = km / 12;                // Carro faz 12 km/L
+            
+            Console.WriteLine(String.Format("{0:0.000}", gas));     // 3 casas decimais
         }
     }
 }
