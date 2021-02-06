42696f2fc1f408548598dc7b611522404ff41b51
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-05 23:37:18 -0300

    Update - Método sacar

--- Banco/Classes/Conta.cs
+++ Banco/Classes/Conta.cs
@@ -2,6 +2,11 @@ namespace Banco.Conta
 {
     public class Conta
     {
-        
+        private TipoConta tipoConta {get; set;}
+
+        private double Saldo {get; set;}
+
+        private double Credito {get; set;}
+        private string Nome {get; set;}
     }
 }
