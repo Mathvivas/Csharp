d3d9e84db6fff20006f1888f9cbab2af25e29ed3
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-05 23:43:27 -0300

    Update

--- Banco/Classes/Conta.cs
+++ Banco/Classes/Conta.cs
@@ -1,12 +1,53 @@
+using System;
+
 namespace Banco.Conta
 {
     public class Conta
     {
-        private TipoConta tipoConta {get; set;}
+        private TipoConta tipoConta { get; set; }
 
-        private double Saldo {get; set;}
+        private double saldo { get; set; }
 
-        private double Credito {get; set;}
-        private string Nome {get; set;}
+        private double credito { get; set; }
+        private string nome { get; set; }
+
+        public Conta (TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.nome = nome;
+            this.saldo = saldo;
+            this.credito = credito;
+            this.tipoConta = tipoConta;
+        }
+
+        public bool sacar (double valorSaque)
+        {
+            // Validação de saldo suficiente
+            if ( this.saldo - valorSaque < (this.credito * -1) )
+            {
+                Console.WriteLine("Saldo Insuficiente");
+                return false;
+            }
+
+            this.saldo -= valorSaque;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.nome, this.saldo);
+
+            return true;
+        }
+
+        public void depositar (double valorDeposito)
+        {
+            this.saldo += valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.nome, this.saldo);
+        }
+
+        public void transferir (double valorTransferencia, Conta contaDestino)
+        {
+            if ( this.sacar(valorTransferencia) )
+            {
+                contaDestino.depositar(valorTransferencia);
+            }
+        }
     }
 }
