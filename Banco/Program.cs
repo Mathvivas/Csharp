9ec1e4b6255c81f08fe9f287de636b8564995802
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-06 00:32:34 -0300

    Update

--- Banco/Program.cs
+++ Banco/Program.cs
@@ -6,50 +6,7 @@ namespace Banco
     {
         static void Main(string[] args)
         {
-            Menu menu = new Menu();
-
-            string op;
-
-            do
-            {
-                op = menu.run();
-
-                switch (op)
-                {
-                    case "1":
-                        listarContas();
-                        break;
-
-                    case "2":
-                        inserirConta();
-                        break;
-
-                    case "3":
-                        transferir();
-                        break;
-                    
-                    case "4":
-                        sacar();
-                        break;
-                    
-                    case "5":
-                        depositar();
-                        break;
-                    
-                    case "6":
-                        Console.Clear();
-                        break;
-
-                    case "0":
-                        Console.WriteLine("Obrigado! Até logo!");
-                        break;
-
-                    default:
-                        Console.WriteLine("Opção Inválida! Tente Novamente!");
-                        break;
-                }
-
-            } while ( op != "0" );
+            
         }
     }
 }
