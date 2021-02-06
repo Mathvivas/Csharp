d8ab4f0352106398608353c5c77159d60e75c8be
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-06 00:25:45 -0300

    Update

--- Banco/Classes/Menu.cs
+++ Banco/Classes/Menu.cs
@@ -4,33 +4,31 @@ namespace Banco
 {
     public class Menu
     {
-        public void run ()
+        public string run ()
         {
             string op;
-            do 
-            {
-                Console.WriteLine("\n|========| Menu |========|");
-                Console.WriteLine("|");
-                Console.WriteLine("| 1 - Listar Contas");
-                Console.WriteLine("|");
-                Console.WriteLine("| 2 - Inserir Nova Conta");
-                Console.WriteLine("|");
-                Console.WriteLine("| 3 - Transferir");
-                Console.WriteLine("|");
-                Console.WriteLine("| 4 - Sacar");
-                Console.WriteLine("|");
-                Console.WriteLine("| 5 - Depositar");
-                Console.WriteLine("|");
-                Console.WriteLine("| 6 - Limpar Tela");
-                Console.WriteLine("|");
-                Console.WriteLine("| 0 - Sair");
-                Console.WriteLine("|");
-                Console.WriteLine("|==========================|");
+            Console.WriteLine("\n|========| Menu |========|");
+            Console.WriteLine("|");
+            Console.WriteLine("| 1 - Listar Contas");
+            Console.WriteLine("|");
+            Console.WriteLine("| 2 - Inserir Nova Conta");
+            Console.WriteLine("|");
+            Console.WriteLine("| 3 - Transferir");
+            Console.WriteLine("|");
+            Console.WriteLine("| 4 - Sacar");
+            Console.WriteLine("|");
+            Console.WriteLine("| 5 - Depositar");
+            Console.WriteLine("|");
+            Console.WriteLine("| 6 - Limpar Tela");
+            Console.WriteLine("|");
+            Console.WriteLine("| 0 - Sair");
+            Console.WriteLine("|");
+            Console.WriteLine("|==========================|");
 
-                Console.WriteLine("\nDigite a opção desejada: ");
-                op = Console.ReadLine();
+            Console.WriteLine("\nDigite a opção desejada: ");
+            op = Console.ReadLine();
 
-            } while ( op != "0" );
+            return op;
         }
     }
 }
