ac7d28cf56f9e2b5a96128e3dbce3d8e22cbbf0c
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-07 09:34:38 -0300

    Criação do Enum Genero

--- Series/Classes/Serie.cs
+++ Series/Classes/Serie.cs
@@ -2,6 +2,10 @@ namespace Series.Classes
 {
     public class Serie : EntidadeBase
     {
-        
+        // Atributos
+        private Genero genero { get; set; }   
+        private string titulo { get; set; }
+        private string descricao { get; set; }
+        private int ano { get; set; }
     }
 }
