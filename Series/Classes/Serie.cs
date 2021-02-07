9af0f003b9dcb44554dcd6bc4238145355c64a35
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-07 09:43:55 -0300

    Update Serie

--- Series/Classes/Serie.cs
+++ Series/Classes/Serie.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Series.Classes
 {
     public class Serie : EntidadeBase
@@ -7,5 +9,35 @@ namespace Series.Classes
         private string titulo { get; set; }
         private string descricao { get; set; }
         private int ano { get; set; }
+
+        // Métodos
+        public Serie (int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.id = id;
+            this.genero = genero;
+            this.titulo = titulo;
+            this.descricao = descricao;
+            this.ano = ano;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.genero + Environment.NewLine;
+            retorno += "Título: " + this.titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.ano;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.titulo;
+        }
+
+        public int retornaId() 
+        {
+            return this.id;
+        }
     }
 }
