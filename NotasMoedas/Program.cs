49b5664dd401fa86ccf5d871fea7d663d9004944
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-22 16:22:22 -0300

    Criação do Projeto Notas e Moedas

---
+++ NotasMoedas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NotasMoedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
