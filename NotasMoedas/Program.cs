a84dd68f6430f6e3ce009d5fd34d2acfa4422863
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-22 16:38:15 -0300

    Update

--- NotasMoedas/Program.cs
+++ NotasMoedas/Program.cs
@@ -6,7 +6,47 @@ namespace NotasMoedas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double valor;
+            int inteiro, auxNotas, auxMoedas;
+
+            Console.WriteLine("Digite um valor qualquer: ");
+            valor = Convert.ToDouble(Console.ReadLine());
+
+            inteiro = (int)valor;
+            valor *= 100;
+            auxMoedas = (int)valor;
+
+
+            Console.WriteLine("\nNOTAS:");
+            Console.WriteLine("{0} nota(s) de R$ 100.00", inteiro/100);
+            auxNotas = (inteiro % 100);
+            //complete o código
+            Console.WriteLine("{0} nota(s) de R$ 50.00", auxNotas/50);
+            auxNotas = (auxNotas % 50);
+            Console.WriteLine("{0} nota(s) de R$ 20.00", auxNotas/20);
+            auxNotas = (auxNotas % 20);
+            Console.WriteLine("{0} nota(s) de R$ 10.00", auxNotas/10);
+            auxNotas = (auxNotas % 10);
+            Console.WriteLine("{0} nota(s) de R$ 5.00", auxNotas/5);
+            auxNotas = (auxNotas % 5);
+            Console.WriteLine("{0} nota(s) de R$ 2.00", auxNotas/2);
+            auxNotas = (auxNotas % 2);
+            
+
+            Console.WriteLine("\nMOEDAS:");
+            Console.WriteLine("{0} moeda(s) de R$ 1.00", auxNotas / 1);
+            auxMoedas %= 100;
+            //complete o codigo
+            Console.WriteLine("{0} moeda(s) de R$ 0.50", auxNotas / 0.5);
+            auxMoedas %= 50 / 100;
+            Console.WriteLine("{0} moeda(s) de R$ 0.25", auxNotas / 0.25);
+            auxMoedas %= 25 / 100;
+            Console.WriteLine("{0} moeda(s) de R$ 0.10", auxNotas / 0.1);
+            auxMoedas %= 10 / 100;
+            Console.WriteLine("{0} moeda(s) de R$ 0.05", auxNotas / 0.05);
+            auxMoedas %= 5 / 100;
+            Console.WriteLine("{0} moeda(s) de R$ 0.01", auxNotas / 0.01);
+
         }
     }
 }
